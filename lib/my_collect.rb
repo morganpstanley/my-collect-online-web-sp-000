858cecc9f76328827004861938ef7fdcef7bdc15
Morgan Stanley <morganpstanley@gmail.com>
2019-07-05 07:47:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,9 @@ def my_collect(array)
   if block_given?
     i = 0
     while i < array.length
-      yield array[i]
+      new_array << yield array[i]
       i += 1
     end
-
   else
     return "No block given, sucka."
 end
