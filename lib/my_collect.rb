bbeb68507442a76f849b9aedae6d972c85a22da8
Morgan Stanley <morganpstanley@gmail.com>
2019-07-05 07:58:04 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    new_array = []
+    while i < array.length
+      new_array << yield(array[i])
+      i += 1
+    end
+    new_array
+  else
+    return "No block given, sucka."
+  end
+end
